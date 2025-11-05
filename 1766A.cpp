3397675f441b4edcb759d777eda90bf65b273e57
Vidhaan Shah <vidhaan05shah@gmail.com>
2025-11-05 10:05:47 +0530

    Fifth Commit

---
+++ 1766A.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <queue>
+#include <unordered_set>
+#include <numeric>
+
+#define all(x) (x).begin(), (x).end()
+#define dbg(x) cout << "Line(" << __LINE__ << ") -> " << #x << " = " << (x) << endl;
+
+using namespace std;
+
+template <typename S, typename T>
+void smax(S &a, const T &b)
+{
+    if (a < b)
+        a = b;
+};
+
+template <typename S, typename T>
+void smin(S &a, const T &b)
+{
+    if (a > b)
+        a = b;
+};
+
+using ll = long long;
+const int INF = 1e9 + 7;
+
+int countDigits(int n){
+    int count=0;
+    while(n>0){
+        count++;
+        n=n/10;
+    }
+    return count;
+}
+void solve() {
+    int n;
+    cin>>n;
+    int count=0;
+    for(int i=1;i<=n;i++){
+        int x=(int)pow(10,countDigits(i));
+        if(i%x==0){
+            count++;
+        }
+    }
+    cout<<count<<endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int TC;
+    cin >> TC;
+    while(TC--) solve(), cout << endl;
+    return 0;
+}
